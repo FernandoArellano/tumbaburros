f3d7395779f4fc79baf916b5f5c9b4a1fb52bf17
fernandoarellano <fer_america20@hotmail.com>
2024-01-29 12:30:25 -0600

    Hacker rank examples

--- src/main/java/com/example/tumbaburros/hackerrank/Test.java
+++ src/main/java/com/example/tumbaburros/hackerrank/Test.java
@@ -464,6 +464,24 @@ public class Test {
         return Integer.parseInt(n);
     }
 
+    //It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position
+    // in the queue from  to . Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker.
+    // One person can bribe at most two others.
+    //Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more
+    // than two people, print Too chaotic.
+    //Example
+    //
+    //If person  bribes person , the queue will look like this: . Only  bribe is required. Print 1.
+    //
+    //Person  had to bribe  people to get to the current position. Print Too chaotic.
+
+    public static void minimumBribes(List<Integer> q) {
+        // Write your code here
+        if(q.size()>=1 && q.size()<=100000){
+
+        }
+    }
+
 
     public static void main(String[] args) throws ParseException {
        // ranks();
