80cd59e9a8744f8a15664f7d88875dabdd1ee0dd
fernandoarellano <fer_america20@hotmail.com>
2024-01-31 15:55:45 -0600

    Hacker rank examples

--- src/main/java/com/example/tumbaburros/hackerrank/Test.java
+++ src/main/java/com/example/tumbaburros/hackerrank/Test.java
@@ -775,6 +775,153 @@ public class Test {
             return -1;
     }
 
+    public static int legoBlocks(int n, int m) {
+        // Write your code here
+        if (n < 2 || m < 1) return 0;
+        if (m == 1) return 1;
+
+        // - Step 1: consider only one row
+        long [] total = new long [m + 1];
+
+        // set a flag (-1) to calculate only once
+        for (int i = 0; i < total.length; i++)
+            total[i] = -1;
+
+        fillTot(total, m);
+
+        // - Step 2: extend to all rows
+        for (int i = 0; i < total.length; i++) {
+            long tmp = 1;
+            for (int j = 0; j < n; j++) {
+                tmp = (tmp * total[i]) % MOD;
+            }
+            total[i] = tmp;
+        }
+
+        // - Step 3: subtract the vertically unstable
+        // don't calculate the vertically unstable at first
+        long [] result = new long [m + 1];
+        // set a flag (-1) to calculate only once
+        for (int i = 0; i < result.length; i++)
+            result[i] = -1;
+
+        getResult(total, result, m);
+
+        // solution 1:
+        // - subtract the vertically unstable
+        // return (int) ((total[m] - result[m]) % MOD);
+
+        // solution 2:
+        // - return the result
+        return (int) (result[m] % MOD);
+    }
+
+    static long MOD = 1000000000 + 7;
+
+    // calculate unstable by splitting it into two parts and
+    // multiplying unstable part with total part
+    static long getResult(long [] total, long [] result, int i) {
+        if (result[i] == -1) {
+            if (i == 1) {
+                // solution 1
+                // result[i] = 0;
+
+                // solution 2
+                result[i] = 1;
+            }
+            else {
+                // solution 1
+                // result[i] = 0;
+                // for (int j = 1; j < i; j++) {
+                //     result[i] += ((total[j] - getResult(total, result, j)) * total[i - j]) % MOD;
+                // }
+
+                // solution 2
+                result[i] = total[i];
+                for (int j = 1; j < i; j++) {
+                    result[i] -= (getResult(total, result, j) * total[i - j]) % MOD;
+                }
+            }
+        }
+
+        return result[i];
+    }
+
+    // fill totals partially
+    static long fillTot(long [] total, int i) {
+        if (i < 0) return 0;
+
+        if (total[i] == -1) {
+            if (i == 0 || i == 1)
+                total[i] = 1;
+            else
+                total[i] = (fillTot(total, i - 1) + fillTot(total, i - 2) + fillTot(total, i - 3) + fillTot(total, i - 4)) % MOD;
+        }
+
+        return total[i];
+    }
+
+    public static List<Integer> searchFirstBreath(int n, int m, List<List<Integer>> edges, int s){
+        if (n < 1 || edges == null || edges.size() < 1 || s < 1) return null;
+
+        // Step 1
+        Set<Integer> [] tree = new Set[n];
+        for (List<Integer> edge : edges) {
+            int a = edge.get(0);
+            int b = edge.get(1);
+            if (tree[a-1] == null) {
+                tree[a-1] = new HashSet<Integer>();
+            }
+            tree[a-1].add(b);
+            if (tree[b-1] == null) {
+                tree[b-1] = new HashSet<Integer>();
+            }
+            tree[b-1].add(a);
+        }
+
+        // Step 2
+        List<Integer> res = new ArrayList<Integer>();
+
+        // O(n) start from S
+        for (int i = 0; i < n - 1; i++) {
+            res.add(-1);
+        }
+
+        Set<Integer> root = tree[s-1];
+        if (root == null) return res;
+
+        Deque<Integer> queue = new LinkedList<Integer>();
+        for (Integer r : root) {
+            queue.add(r);
+        }
+        int height = 1;
+        boolean [] visited = new boolean [n];
+        visited[s-1] = true;
+        while (!queue.isEmpty()) {
+            Deque<Integer> queuen = new LinkedList<Integer>();
+            while (!queue.isEmpty()) {
+                int v = queue.poll().intValue();
+                if (!visited[v-1]) {
+                    visited[v-1] = true;
+                    // update distances
+                    if (v > s)
+                        res.set(v-2, height*6);
+                    else
+                        res.set(v-1, height*6);
+                    if (tree[v-1] != null) {
+                        for (Integer r : tree[v-1]) {
+                            queuen.add(r);
+                        }
+                    }
+                }
+            }
+            queue = queuen;
+            height++;
+        }
+
+        // return res
+        return res;
+    }
 
 
 
@@ -871,6 +1018,13 @@ public class Test {
         //simpleTextEditor();
 
         System.out.println(cookies(9,Arrays.asList(2,7,3,6,4,6)));
+
+        System.out.println(legoBlocks(2,3));
+
+        List<List<Integer>> searchList = new LinkedList<>();
+        searchList.add(new ArrayList<>(){{add(1);add(2);}});
+        searchList.add(new ArrayList<>(){{add(1);add(3);}});
+        System.out.println(searchFirstBreath(4,2, searchList, 1));
     }
 
 
