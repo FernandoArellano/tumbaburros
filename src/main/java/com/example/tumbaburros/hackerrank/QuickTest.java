1727bcc8c35aec335381e9e91c9987fc122e0dab
FernandoArellano <fer_america20@outlook.com>
2024-07-03 19:42:21 -0600

    study

--- src/main/java/com/example/tumbaburros/hackerrank/QuickTest.java
+++ src/main/java/com/example/tumbaburros/hackerrank/QuickTest.java
@@ -1,47 +1,68 @@
 package com.example.tumbaburros.hackerrank;
 
+import java.util.*;
+
 public class QuickTest {
 
     /*
-    Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
-    Return the number of good nodes in the binary tree.
+        You are given two 0-indexed integer arrays nums1 and nums2 of equal length n and a positive integer k.
+        You must choose a subsequence of indices from nums1 of length k.
+        For chosen indices i0, i1, ..., ik - 1, your score is defined as:
+        The sum of the selected elements from nums1 multiplied with the minimum of the selected elements from nums2.
+        It can defined simply as: (nums1[i0] + nums1[i1] +...+ nums1[ik - 1]) * min(nums2[i0] , nums2[i1], ... ,nums2[ik - 1]).
+        Return the maximum possible score.
 
-    times node value is greater or equal to max value
+        Input: nums1 = [1,3,3,2], nums2 = [2,1,3,4], k = 3
+        Output: 12
+        Explanation:
+        The four possible subsequence scores are:
+        - We choose the indices 0, 1, and 2 with score = (1+3+3) * min(2,1,3) = 7.
+        - We choose the indices 0, 1, and 3 with score = (1+3+2) * min(2,1,4) = 6.
+        - We choose the indices 0, 2, and 3 with score = (1+3+2) * min(2,3,4) = 12.
+        - We choose the indices 1, 2, and 3 with score = (3+3+2) * min(1,3,4) = 8.
+        Therefore, we return the max score, which is 12.
 
-        TreeNode four = new TreeNode(4);
-        TreeNode two = new TreeNode(2);
-
-        TreeNode three1 = new TreeNode(3,four, two);
-
-        TreeNode three = new TreeNode(3, three1, null);
-
-        System.out.println(goodNodes(three));
-
+       al ordenar el q se multiplica de mayor a menor, aseguras q se multiplique por el menor de los q se estan usando
+       al hacer pop al mas pequeño quedas con los mas grandes  para la suma
+       cuidado con el tipo de dato, el int no llega a sumas altas
      */
-    public static int counter =0;
+    public static long maxScore(int[] nums1, int[] nums2, int k) {
+        long ans =0;
+        List<int[]> list = new LinkedList<>();
 
-    public static int goodNodes(TreeNode root) {
+        if(nums1.length == nums2.length){
 
-        Integer maxValue = Integer.MIN_VALUE;
-        fillMaxVal(root, maxValue);
+            if(nums1.length==1){
+                return nums1[0]*nums2[0];
+            }
 
 
-        return counter;
+
+            for(int i=0; i<nums1.length;i++){
+                list.add(new int[]{nums1[i],nums2[i]});
+            }
+
+            Collections.sort(list, (a, b) -> b[1]-a[1]);
+
+            long sum=0;
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+            for(int[] array : list){
+                sum+=array[0];
+
+                if(queue.size()==k){
+                    sum-= queue.poll();
+                }
+                if(queue.size()== k-1){
+                    ans = Math.max(ans, sum*array[1]);
+                }
+                queue.add(array[0]);
+            }
+        }
+
+      return ans;
     }
 
-    public static void fillMaxVal(TreeNode tree, Integer maxValue){
-        if(tree.val>=maxValue){
-            counter++;
-            maxValue = tree.val;
-        }
-
-        if(tree.left != null){
-            fillMaxVal(tree.left, maxValue);
-        }
-        if(tree.right != null){
-            fillMaxVal(tree.right, maxValue);
-        }
-    }
 
     static class TreeNode {
       int val;
@@ -65,13 +86,6 @@ public class QuickTest {
   }
 
     public static void main(String[] args) {
-        TreeNode four = new TreeNode(4);
-        TreeNode two = new TreeNode(2);
-
-        TreeNode three1 = new TreeNode(3,four, two);
-
-        TreeNode three = new TreeNode(3, three1, null);
-
-        System.out.println(goodNodes(three));
+        System.out.println(maxScore(new int[]{1,3,3,2}, new int[]{2,1,3,4},3));
     }
 }
--- src/main/java/com/example/tumbaburros/hackerrank/Test.java
+++ src/main/java/com/example/tumbaburros/hackerrank/Test.java
@@ -2278,6 +2278,93 @@ public class Test {
         }
     }
 
+    /*
+        Given an integer array nums and an integer k, return the kth largest element in the array.
+        Note that it is the kth largest element in the sorted order, not the kth distinct element.
+        Can you solve it without sorting?
+
+        System.out.println(findKthLargest(new int[]{3,2,1,5,6,4},2));
+     */
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        if(nums.length==0){
+            return 0;
+        }
+
+        for(int i=0; i< nums.length; i++){
+            queue.add(nums[i]);
+        }
+
+        for(int i=1; i<k; i++){
+            queue.poll();
+        }
+
+
+        return queue.poll();
+    }
+
+
+    /*
+    You are given two 0-indexed integer arrays nums1 and nums2 of equal length n and a positive integer k.
+    You must choose a subsequence of indices from nums1 of length k.
+    For chosen indices i0, i1, ..., ik - 1, your score is defined as:
+    The sum of the selected elements from nums1 multiplied with the minimum of the selected elements from nums2.
+    It can defined simply as: (nums1[i0] + nums1[i1] +...+ nums1[ik - 1]) * min(nums2[i0] , nums2[i1], ... ,nums2[ik - 1]).
+    Return the maximum possible score.
+
+    Input: nums1 = [1,3,3,2], nums2 = [2,1,3,4], k = 3
+    Output: 12
+    Explanation:
+    The four possible subsequence scores are:
+    - We choose the indices 0, 1, and 2 with score = (1+3+3) * min(2,1,3) = 7.
+    - We choose the indices 0, 1, and 3 with score = (1+3+2) * min(2,1,4) = 6.
+    - We choose the indices 0, 2, and 3 with score = (1+3+2) * min(2,3,4) = 12.
+    - We choose the indices 1, 2, and 3 with score = (3+3+2) * min(1,3,4) = 8.
+    Therefore, we return the max score, which is 12.
+
+   al ordenar el q se multiplica de mayor a menor, aseguras q se multiplique por el menor de los q se estan usando
+   al hacer pop al mas pequeño quedas con los mas grandes  para la suma
+   cuidado con el tipo de dato, el int no llega a sumas altas
+ */
+    public static long maxScore(int[] nums1, int[] nums2, int k) {
+        long ans =0;
+        List<int[]> list = new LinkedList<>();
+
+        if(nums1.length == nums2.length){
+
+            if(nums1.length==1){
+                return nums1[0]*nums2[0];
+            }
+
+
+
+            for(int i=0; i<nums1.length;i++){
+                list.add(new int[]{nums1[i],nums2[i]});
+            }
+
+            Collections.sort(list, (a, b) -> b[1]-a[1]);
+
+            long sum=0;
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+            for(int[] array : list){
+                sum+=array[0];
+
+                if(queue.size()==k){
+                    sum-= queue.poll();
+                }
+                if(queue.size()== k-1){
+                    ans = Math.max(ans, sum*array[1]);
+                }
+                queue.add(array[0]);
+            }
+        }
+
+        return ans;
+    }
+
     static class ListNode {
         int val;
         QuickTest.ListNode next;
