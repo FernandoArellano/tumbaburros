a6fd9b1e0e974de92b369bc6a85fb05e897708ae
FernandoArellano <fer_america20@outlook.com>
2025-05-19 19:13:11 -0600

    study

--- src/main/java/com/example/tumbaburros/hackerrank/QuickTest.java
+++ src/main/java/com/example/tumbaburros/hackerrank/QuickTest.java
@@ -8,6 +8,7 @@ import java.util.function.Function;
 public class QuickTest {
 
 
+
   static class TreeNode {
       int val;
       TreeNode left;
@@ -31,5 +32,18 @@ public class QuickTest {
 
     public static void main(String[] args) {
 
+        Map<String, List<String>> people = new HashMap<>();
+
+        people.put("John", Arrays.asList("555-1123", "555-3389"));
+
+        people.put("Mary", Arrays.asList("555-2243", "555-5264"));
+
+        people.put("Steve", Arrays.asList("555-6654", "555-3242"));
+
+        List<String> phones = people.values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        System.out.println(phones);
+       
     }
 }
