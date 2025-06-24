12e059f909b96311326055b125b410ee8c7bc350
FernandoArellano <fer_america20@outlook.com>
2025-06-24 16:15:08 -0600

    study

---
+++ src/main/java/com/example/tumbaburros/exercizes/Main.java
@@ -0,0 +1,127 @@
+package com.example.tumbaburros.exercizes;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void line(){
+        System.out.println("-------------------------------------------------------------------------------------");
+    }
+
+//    Dado: Lista de palabras
+//    Objetivo: Agrupar anagramas entre sí.
+//
+//    java
+//            Copiar
+//    Editar
+//    List<String> input = List.of("eat", "tea", "tan", "ate", "nat", "bat");
+//    Salida esperada:
+//
+//    java
+//            Copiar
+//    Editar
+//[
+//        [eat, tea, ate],
+//            [tan, nat],
+//            [bat]
+//            ]
+
+    public static void groupAnagram(){
+        List<String> input = List.of("eat", "tea", "tan", "ate", "nat", "bat");
+        Map<String, Set<String>> map = new LinkedHashMap<>();
+
+        for(String s: input){
+
+            char[] array = s.toCharArray();
+            Arrays.sort(array);
+            String ordered = new String(array);
+
+            if(map.containsKey(ordered)){
+                Set set = map.get(ordered);
+                set.add(s);
+            } else{
+                Set set = new LinkedHashSet();
+                set.add(s);
+                map.put(ordered, set);
+            }
+        }
+
+        if(!map.isEmpty()){
+            Set<String> finalResult = new LinkedHashSet<>();
+            for(Map.Entry<String, Set<String>> entry: map.entrySet()){
+                Set<String> entryValue = entry.getValue();
+                String resultString = entryValue.stream().collect(Collectors.joining(", "));
+                finalResult.add(resultString);
+            }
+
+            for(String s : finalResult){
+                System.out.println("["+s+"]");
+            }
+
+        }
+
+    }
+
+//    2. Stream Word Frequency (★★★)
+//    Dado: Stream<String> de millones de palabras
+//    Objetivo: Obtener las k palabras más frecuentes.
+    public static void moreFrequentWord(Stream<String> stream, int k){
+        Map<String, Long> frequence = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        LinkedHashSet<String> set = frequence.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+                .limit(k)
+                .map(entry->entry.getKey())
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        System.out.println(set);
+    }
+
+//    3. Longest Consecutive Sequence (★★★★)
+//    Dado: Array de enteros no ordenado
+//    Objetivo: Encontrar la longitud de la subsecuencia consecutiva más larga.
+//
+//    Idea clave O(n) con HashSet
+//    Mete todos los números en un HashSet<Integer> → búsquedas O(1).
+//
+//    Recorre cada número; sólo arrancas una exploración si es posible inicio de una secuencia (es decir, num‑1 no está en el set).
+//
+//    Mientras num+len esté en el set vas contando (len++).
+//
+//    Vas actualizando el máximo.
+//
+//    Así evitas ordenar (que costaría O(nlogn)) y mantienes O(n) tiempo / O(n) espacio.
+    public static void longestSequence(Set<Integer> set){
+        Integer max = Integer.MIN_VALUE;
+        int longest=0;
+        for(Integer i: set){
+            if(i>max){
+                max = i;
+            }
+        }
+
+        int tempLongest=0;
+        for(int i=0; i<=max; i++){
+            if(set.contains(i)){
+                tempLongest++;
+                if(tempLongest>longest){
+                    longest=tempLongest;
+                }
+            }else{
+                tempLongest=0;
+            }
+        }
+        System.out.println(longest);
+    }
+
+
+    public static void main(String[] args) {
+        groupAnagram();line();
+        moreFrequentWord(Stream.of("apple", "banana", "apple", "orange", "banana", "apple"), 2);line();
+        longestSequence(Set.of(100, 4, 200, 1, 3, 2,5,6,7));line();
+    }
+
+}
--- src/main/java/com/example/tumbaburros/streams/Main.java
+++ src/main/java/com/example/tumbaburros/streams/Main.java
@@ -1,8 +1,7 @@
 package com.example.tumbaburros.streams;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -44,6 +43,82 @@ public class Main {
         Stream<Integer> iterate = Stream.iterate(2, num -> num<50, num -> num*=num);
         System.out.println(iterate.collect(Collectors.toList()));
 
-        //reduce
+        //joining strings
+        List<String> grupos = List.of("Banda A", "Banda B", "Banda C");
+        StringJoiner joiner = new StringJoiner(",");
+        StringBuilder sb = new StringBuilder();
+
+        grupos.stream().forEach(joiner::add);
+
+        System.out.println(sb.append(joiner));
+
+        //better way
+        String result = grupos.stream().collect(Collectors.joining(","));
+        System.out.println(result);
+
+        //int stream sum
+        List<Integer> enteros = List.of(1, 2, 3, 4, 5);
+        System.out.println(enteros.stream().mapToInt(i-> i).sum());
+
+        //grouping by
+        List<String> correos = List.of("foo@bar.com", "baz@bar.com", "foo@baz.com");
+        Map<String, List<String>> collect = correos.stream().collect(Collectors.groupingBy(c -> c.split("@")[1]));
+        System.out.println(collect);
+
+        //longest word comparing method
+        List<String> palabras = List.of("stream", "java", "colecciones", "api");
+        String longestword = palabras.stream().max(Comparator.comparing(String::length)).orElse("");
+        System.out.println(longestword);
+
+        //flatmap
+        List<List<String>> listas = List.of(List.of("a", "b"), List.of("c", "d"));
+        List<String> aplanada = listas.stream().flatMap(List::stream).collect(Collectors.toList());
+        System.out.println(aplanada);
+
+        //collectors grouping by
+        List<String> palabras2 = List.of("java", "stream", "java", "code");
+        System.out.println(palabras2.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
+
+        //collect people by age
+        // personas.stream().collect(Collectors.groupingBy(Persona::getEdad))
+
+        //lista de personas a mapa de personas con edad
+        // personas.stream().collect(Collectors.toMap(Persona::getNombre, Persona::getEdad))
+
+        //agrupar por inicial
+        List<String> nombres = List.of("Ana", "Alma", "Bruno", "Beto");
+        System.out.println(nombres.stream().collect(Collectors.groupingBy(s-> s.toCharArray()[0])));
+
+        //sumar por concepto
+        // compras.stream().collect(Collectors.groupingBy(Compra::getCategoria, Collectors.summingDouble(Compra::getMonto)))
+
+        //second biggest
+        // nums.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElse(-1)
+
+        //convert into sorted set
+        List<Integer> nums = List.of(3, 1, 4, 2, 3);
+        System.out.println(nums.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toSet()));
+
+        //group by length and counting
+        List<String> palabras3 = List.of("uno", "dos", "tres", "cuatro");
+        System.out.println(palabras3.stream().collect(Collectors.groupingBy(String::length, Collectors.counting())));
+
+        //reduce suma de los cuadrados
+        List<Integer> nums2 = List.of(2, 3, 4);
+        System.out.println(nums2.stream().map(n -> n * n).reduce(0, Integer::sum));
+
+        //multiply all elements (reduce)
+        List<Integer> valores = List.of(2, 3, 4);
+        System.out.println(valores.stream().reduce(1, (a, b) -> a * b));
+
+        //get map initial and longest word of that initial
+        List<String> nombres3 = List.of("Ana", "Alberto", "Bruno", "Benito");
+
+        System.out.println(nombres3.stream()
+                .collect(Collectors.groupingBy(n -> n.charAt(0),
+                      Collectors.collectingAndThen(
+                      Collectors.maxBy(Comparator.comparingInt(String::length)),
+                      Optional::get))));
+
     }
 }
