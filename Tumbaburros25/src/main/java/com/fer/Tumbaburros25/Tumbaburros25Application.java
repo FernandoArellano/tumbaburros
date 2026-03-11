da73e5922292bc3987f8f3e13013b54a9d7e8b96
FernandoArellano <fer_america20@outlook.com>
2026-03-11 10:04:49 -0600

    study

---
+++ Tumbaburros25/src/main/java/com/fer/Tumbaburros25/Tumbaburros25Application.java
@@ -0,0 +1,13 @@
+package com.fer.Tumbaburros25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tumbaburros25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tumbaburros25Application.class, args);
+	}
+
+}
---
+++ Tumbaburros25/src/test/java/com/fer/Tumbaburros25/Tumbaburros25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.Tumbaburros25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tumbaburros25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
